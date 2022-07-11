970f26e8b03d6d84ec0168b543adeabe0a7404f2
malyq <maxim99112@gmail.com>
2022-07-11 17:53:56 +0500

    add screens object

---
+++ app/src/main/java/com/example/myapplicationapi/Screens/Screens.kt
@@ -0,0 +1,8 @@
+package com.example.myapplicationapi.Screens
+
+import com.example.myapplicationapi.DetailFragment.DetailFragment
+import com.example.myapplicationapi.imageViewFargment.ImageFragment
+
+object Screens {
+
+}
