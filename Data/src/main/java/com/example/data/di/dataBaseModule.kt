eb65ac8a250f6bf13a06ee8986b57d4a5d89cdfc
malyq <maxim99112@gmail.com>
2022-07-22 14:40:18 +0500

    .

--- Data/src/main/java/com/example/data/di/dataBaseModule.kt
+++ Data/src/main/java/com/example/data/di/dataBaseModule.kt
@@ -1,8 +1,6 @@
 package com.example.data.di
 
-import androidx.lifecycle.ViewModel
 import com.example.data.repositories.SomethingRepository
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val dataBaseModule = module {
